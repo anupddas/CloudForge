5a59e5cdc9c6390020be9018465c8875668cb73d
anupddas <anupddas8@gmail.com>
2026-06-02 22:03:23 +0530

    bootstrap terraform backend infrastructure

---
+++ bootstrap/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+
+  bucket = "cloudforge-tfstate-847110629980"
+
+  tags = {
+    Name        = "Terraform State"
+    Environment = "bootstrap"
+    Project     = "CloudForge"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+
+  name         = "cloudforge-terraform-locks"
+
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Project = "CloudForge"
+  }
+}
